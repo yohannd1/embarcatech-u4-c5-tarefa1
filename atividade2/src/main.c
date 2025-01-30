f1b24ea8090a0f161b39215462930f2b7326611e
yohannd1 <mitonanan12@gmail.com>
2025-01-30 01:43:59 -0300

    atividade 2: implementação do LED temporizado

--- atividade2/src/main.c
+++ atividade2/src/main.c
@@ -6,54 +6,53 @@
 #define BUTTON_PIN 5
 #define DEBOUNCING_TIME_US 200000
 
-#define LED_RED_PIN 11
-#define LED_YELLOW_PIN 12
-#define LED_GREEN_PIN 13
+#define LED_PIN_1 11
+#define LED_PIN_2 12
+#define LED_PIN_3 13
 #define INTERVAL_MS 3000
 
 static volatile uint8_t counter = 0;
+static volatile bool is_alarm_enabled = false;
 
 static void handle_button_press(uint gpio, uint32_t events);
 static bool update_semaphore(struct repeating_timer *_);
 
+int64_t callback(alarm_id_t _id, void *_) {
+	gpio_put(LED_PIN_1, counter <= 0);
+	gpio_put(LED_PIN_2, counter <= 1);
+	gpio_put(LED_PIN_3, counter <= 2);
+
+	printf("* %d\n", counter);
+	is_alarm_enabled = (counter < 3);
+
+	counter++;
+	return is_alarm_enabled ? (INTERVAL_MS * 1000) : 0;
+}
+
 int main() {
     stdio_init_all();
 
-	/* gpio_init(LED_RED_PIN); */
-	/* gpio_set_dir(LED_RED_PIN, GPIO_OUT); */
-	/* gpio_put(LED_RED_PIN, 0); */
+	gpio_init(LED_PIN_1);
+	gpio_set_dir(LED_PIN_1, GPIO_OUT);
 
-	/* gpio_init(LED_YELLOW_PIN); */
-	/* gpio_set_dir(LED_YELLOW_PIN, GPIO_OUT); */
-	/* gpio_put(LED_YELLOW_PIN, 0); */
+	gpio_init(LED_PIN_2);
+	gpio_set_dir(LED_PIN_2, GPIO_OUT);
 
-	/* gpio_init(LED_GREEN_PIN); */
-	/* gpio_set_dir(LED_GREEN_PIN, GPIO_OUT); */
-	/* gpio_put(LED_GREEN_PIN, 0); */
+	gpio_init(LED_PIN_3);
+	gpio_set_dir(LED_PIN_3, GPIO_OUT);
+
+	gpio_put(LED_PIN_1, 0);
+	gpio_put(LED_PIN_2, 0);
+	gpio_put(LED_PIN_3, 0);
 
 	gpio_init(BUTTON_PIN);
 	gpio_set_dir(BUTTON_PIN, GPIO_IN);
 	gpio_pull_up(BUTTON_PIN);
 	gpio_set_irq_enabled_with_callback(BUTTON_PIN, GPIO_IRQ_EDGE_FALL, true, &handle_button_press);
 
-	/* struct repeating_timer timer; */
-	/* add_repeating_timer_ms(INTERVAL_MS, update_semaphore, NULL, &timer); */
-
-	while (true) {
-		printf("Olá, mundo! %d\n", counter);
-		sleep_ms(1000);
-	}
+	while (true)
+		sleep_ms(10000);
 }
-
-/* static bool update_semaphore(struct repeating_timer *_) { */
-/* 	gpio_put(LED_RED_PIN, counter == 0); */
-/* 	gpio_put(LED_YELLOW_PIN, counter == 1); */
-/* 	gpio_put(LED_GREEN_PIN, counter == 2); */
-
-/* 	counter = (counter + 1) % 3; */
-
-/* 	return true; */
-/* } */
 
 static void handle_button_press(uint gpio, uint32_t events) {
 	static volatile uint32_t last_time = 0;
@@ -62,8 +61,10 @@ static void handle_button_press(uint gpio, uint32_t events) {
     uint32_t current_time = to_us_since_boot(get_absolute_time());
 
 	if (current_time - last_time > DEBOUNCING_TIME_US) {
-		if (!gpio_get(BUTTON_PIN)) {
-			counter++;
+		if (!gpio_get(BUTTON_PIN) && !is_alarm_enabled) {
+			counter = 0;
+			is_alarm_enabled = true;
+			add_alarm_in_ms(1, callback, NULL, false);
 		}
 		last_time = current_time;
 	}
