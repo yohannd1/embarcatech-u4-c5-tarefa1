a225380d80df83d9087fcd17cccc9324948afe6f
yohannd1 <mitonanan12@gmail.com>
2025-01-30 00:28:51 -0300

    atividade 1: implementação rudimentar do semáforo

--- atividade1/src/main.c
+++ atividade1/src/main.c
@@ -3,14 +3,38 @@
 
 #include "pico/stdlib.h"
 
+#define LED_RED_PIN 11
+#define LED_YELLOW_PIN 12
+#define LED_GREEN_PIN 13
+#define INTERVAL_MS 3000
+
 static void die(const char *msg);
 
 int main() {
     stdio_init_all();
 
+	gpio_init(LED_RED_PIN);
+	gpio_set_dir(LED_RED_PIN, GPIO_OUT);
+
+	gpio_init(LED_YELLOW_PIN);
+	gpio_set_dir(LED_YELLOW_PIN, GPIO_OUT);
+
+	gpio_init(LED_GREEN_PIN);
+	gpio_set_dir(LED_GREEN_PIN, GPIO_OUT);
+
 	while (true) {
-		printf("Teste\n");
-		sleep_ms(1000);
+		gpio_put(LED_RED_PIN, 1);
+		gpio_put(LED_YELLOW_PIN, 0);
+		gpio_put(LED_GREEN_PIN, 0);
+		sleep_ms(INTERVAL_MS);
+
+		gpio_put(LED_RED_PIN, 0);
+		gpio_put(LED_YELLOW_PIN, 1);
+		sleep_ms(INTERVAL_MS);
+
+		gpio_put(LED_YELLOW_PIN, 0);
+		gpio_put(LED_GREEN_PIN, 1);
+		sleep_ms(INTERVAL_MS);
 	}
 }
 
