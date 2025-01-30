d3dec14e1a7c741f7cee9ba3bb9673edad3a85af
yohannd1 <mitonanan12@gmail.com>
2025-01-30 00:53:17 -0300

    atividade 1: implementando timer via hardware_timer

--- atividade1/src/main.c
+++ atividade1/src/main.c
@@ -8,39 +8,40 @@
 #define LED_GREEN_PIN 13
 #define INTERVAL_MS 3000
 
-static void die(const char *msg);
+static volatile uint8_t counter = 0;
+
+static bool update_semaphore(struct repeating_timer *_);
 
 int main() {
     stdio_init_all();
 
 	gpio_init(LED_RED_PIN);
 	gpio_set_dir(LED_RED_PIN, GPIO_OUT);
+	gpio_put(LED_RED_PIN, 0);
 
 	gpio_init(LED_YELLOW_PIN);
 	gpio_set_dir(LED_YELLOW_PIN, GPIO_OUT);
+	gpio_put(LED_YELLOW_PIN, 0);
 
 	gpio_init(LED_GREEN_PIN);
 	gpio_set_dir(LED_GREEN_PIN, GPIO_OUT);
+	gpio_put(LED_GREEN_PIN, 0);
+
+	struct repeating_timer timer;
+	add_repeating_timer_ms(INTERVAL_MS, update_semaphore, NULL, &timer);
 
 	while (true) {
-		gpio_put(LED_RED_PIN, 1);
-		gpio_put(LED_YELLOW_PIN, 0);
-		gpio_put(LED_GREEN_PIN, 0);
-		sleep_ms(INTERVAL_MS);
-
-		gpio_put(LED_RED_PIN, 0);
-		gpio_put(LED_YELLOW_PIN, 1);
-		sleep_ms(INTERVAL_MS);
-
-		gpio_put(LED_YELLOW_PIN, 0);
-		gpio_put(LED_GREEN_PIN, 1);
-		sleep_ms(INTERVAL_MS);
+		printf("Olá, mundo!\n");
+		sleep_ms(1000);
 	}
 }
 
-static void die(const char *msg) {
-	while (true) {
-		printf("ERRO FATAL: %s\n", msg);
-		sleep_ms(2000);
-	}
+static bool update_semaphore(struct repeating_timer *_) {
+	gpio_put(LED_RED_PIN, counter == 0);
+	gpio_put(LED_YELLOW_PIN, counter == 1);
+	gpio_put(LED_GREEN_PIN, counter == 2);
+
+	counter = (counter + 1) % 3;
+
+	return true;
 }
